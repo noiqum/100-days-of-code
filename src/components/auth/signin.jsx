70aecf61aa5a0f304c291a12eb8cf2e8c39b3f1e
unknown <onurcoskun99@gmail.com>
2020-08-27 23:28:09 +0300

    signin google button added

--- src/components/auth/signin.jsx
+++ src/components/auth/signin.jsx
@@ -1,9 +1,75 @@
 import React from "react";
-
+import { ReactComponent as SvgAuth } from "../../sass/svg/auth.svg";
+import { useState } from "react";
+import { ReactComponent as SvgGoogle } from "../../sass/svg/google.svg";
+//style=signin.scss
 function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const inputHandler = (e) => {
+    switch (e.target.name) {
+      case "email":
+        return setEmail(e.target.value);
+      case "password":
+        return setPassword(e.target.value);
+
+      default:
+        break;
+    }
+  };
+  const submitHandler = (e) => {
+    e.preventDefault();
+  };
+  const googleSignIn = () => {
+    console.log("gogle");
+  };
+
   return (
-    <div>
-      <h4>Sign in</h4>
+    <div className="signin">
+      <SvgAuth />
+      <h2>Sign In</h2>
+
+      <div className="signin__img"></div>
+
+      <form className="signin__form" onSubmit={submitHandler}>
+        <button className="signin__form__google" onClick={googleSignIn}>
+          <SvgGoogle />
+          <span>Sign In With Google</span>
+        </button>
+        <span>Or</span>
+        <div>
+          <label htmlFor="email" className="signin__form__label-email">
+            Email
+          </label>
+          <input
+            name="email"
+            type="email"
+            className="signin__form__input-email"
+            value={email}
+            onChange={inputHandler}
+          />
+        </div>
+        <div>
+          <label htmlFor="password" className="signin__form__label-password">
+            Password
+          </label>
+          <input
+            type="password"
+            name="password"
+            onChange={inputHandler}
+            className="signin__form__input-password"
+            value={password}
+          />
+        </div>
+        <div>
+          <input
+            type="submit"
+            className="signin__form__submit"
+            value="Sign In"
+          />
+        </div>
+      </form>
     </div>
   );
 }
---
+++ src/firebaseConfig/firebaseConfig.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase/app';
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAAyw-5tP9uk0Wh5kgqqSIu3MdosCq69o8",
+    authDomain: "coding-challenge-29d43.firebaseapp.com",
+    databaseURL: "https://coding-challenge-29d43.firebaseio.com",
+    projectId: "coding-challenge-29d43",
+    storageBucket: "coding-challenge-29d43.appspot.com",
+    messagingSenderId: "1014741877640",
+    appId: "1:1014741877640:web:279b7786a2963e9f74303d"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export const provider = new firebase.auth.GoogleAuthProvider();
