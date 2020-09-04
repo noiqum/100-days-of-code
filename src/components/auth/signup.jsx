3316b91d24bd222c991fc31ad21471d0dc6b0ba5
unknown <onurcoskun99@gmail.com>
2020-09-04 09:25:01 +0300

    signup page layout done

--- src/components/auth/signup.jsx
+++ src/components/auth/signup.jsx
@@ -2,8 +2,30 @@ import React from "react";
 
 function SignUp() {
   return (
-    <div>
-      <h4>Sign up</h4>
+    <div className="signup">
+      <h2>Register</h2>
+      <div className="signup__img">Register</div>
+      <form className="signup__form">
+        <div className="signup__form__name">
+          <label htmlFor="name">Display Name</label>
+          <input name="name" type="text" />
+        </div>
+        <div className="signup__form__email">
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" />
+        </div>
+        <div className="signup__form__password">
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" />
+        </div>
+        <div className="signup__form__password2">
+          <label htmlFor="password2">Password Repeat</label>
+          <input type="password" />
+        </div>
+        <div className="signup__form__submit">
+          <input type="submit" value="Register" />
+        </div>
+      </form>
     </div>
   );
 }
