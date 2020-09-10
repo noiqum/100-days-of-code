3703995be8536a7abf07dca5b0504831daac22ca
unknown <onurcoskun99@gmail.com>
2020-09-10 17:29:19 +0300

    tweet link added to card component

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Tasks from "./tasks";
 import { motion } from "framer-motion";
+import { ReactComponent as Twitter } from "../../sass/svg/twitter.svg";
 //style=card.scss
 
 function Card({ count, tasks, title, finished }) {
@@ -32,6 +33,15 @@ function Card({ count, tasks, title, finished }) {
         {tasks.length} {tasks.length > 1 ? "tasks" : "task"}
       </div>
       {<Tasks tasks={tasks} count={count} />}
+      {finished && (
+        <a
+          href={`https://twitter.com/intent/tweet?text=Day${count}&hashtags=100DaysOfCode`}
+          target="_blank"
+          className="card__tweet"
+        >
+          share your journey <Twitter />
+        </a>
+      )}
     </motion.div>
   );
 }
